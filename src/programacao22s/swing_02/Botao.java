cadfc77a8b29f7c80668b9fdb7c0d6f4f4847337
Your Name <you@example.com>
2020-09-16 19:51:15 -0300

    figura

--- src/programacao22s/swing_02/Botao.java
+++ src/programacao22s/swing_02/Botao.java
@@ -1,13 +1,19 @@
 package programacao22s.swing_02;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Botao {
 	public static void main(String[] args) {
+		
+		/// Lembrando que o Framework Swing usa o padrão de projeto Composite.
 		JButton b1 = new JButton("Abrir XML");
-		JButton b2 = new JButton("B1");
+		
+		ImageIcon img = new ImageIcon("laranja.jpg");
+		
+		JButton b2 = new JButton(img);
 		
 		JPanel painel = new JPanel();
 		painel.add(b1);
