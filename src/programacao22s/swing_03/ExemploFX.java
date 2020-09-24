4328cde475afdfd1fc0612a62efebc266b921527
Your Name <you@example.com>
2020-09-23 22:04:17 -0300

    thread

---
+++ src/programacao22s/swing_03/ExemploFX.java
@@ -0,0 +1,45 @@
+package programacao22s.swing_03;
+
+import javax.swing.JOptionPane;
+//
+//import javafx.application.Application;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
+
+//https://www.youtube.com/watch?v=5KyYAsYvLjs
+//https://www.youtube.com/watch?v=57zkzBE0g48
+//https://www.youtube.com/watch?v=2j-e1CHsqsE    - XML
+public class ExemploFX 
+//extends Application 
+{
+//	@Override
+//	public void start(Stage tela) throws Exception {
+//		Button botao = new Button("Iniciar");
+//		botao.setOnAction(new EventHandler<ActionEvent>() {
+//			@Override
+//			public void handle(ActionEvent event) {
+//				System.err.println("Ola Mundo");
+//			}
+//		});
+//
+//		botao.setOnAction(sergio -> {
+//			System.out.println(sergio.toString());
+//			JOptionPane.showConfirmDialog(null, "Usei classe anonima");
+//			System.out.println("clique no bota verde");
+//		});
+//
+//		StackPane root = new StackPane();
+//		root.getChildren().add(botao);
+//		Scene cena = new Scene(root, 500, 300);
+//		tela.setScene(cena);
+//		tela.show();
+//	}
+//
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+}
---
+++ src/programacao22s/swing_03/ExemploThread.java
@@ -0,0 +1,49 @@
+package programacao22s.swing_03;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ExemploThread extends JFrame {
+	JPanel panel;
+	JButton botaoIniciar;
+	JTextField hora;
+
+	public void criarJanela() {
+		this.panel = new JPanel();
+		this.hora = new JTextField(new Date().toString());
+		
+		this.botaoIniciar = new JButton("Iniciar");
+		this.botaoIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				 Relogio r = new Relogio(hora);
+				 r.start();
+			}
+		});
+		this.panel.add(this.hora);
+		this.panel.add(this.botaoIniciar);
+		this.panel.setLayout(new GridLayout(0, 1));
+		Container x = getContentPane();
+		x.add(this.panel, BorderLayout.CENTER);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(620, 400); // largura , 2 - altura
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		 new ExemploThread().processar();
+	}
+
+	public void processar() {
+		criarJanela();
+	}
+}
