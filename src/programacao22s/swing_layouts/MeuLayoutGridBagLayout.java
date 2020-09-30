31f3900813678355183d2828eaf1a51c3dcc5758
Your Name <you@example.com>
2020-09-30 20:55:11 -0300

    gridbag

--- src/programacao22s/swing_layouts/MeuLayoutGridBagLayout.java
+++ src/programacao22s/swing_layouts/MeuLayoutGridBagLayout.java
@@ -1,8 +1,6 @@
 package programacao22s.swing_layouts;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Panel;
 
 import javax.swing.JButton;
@@ -10,21 +8,34 @@ import javax.swing.JFrame;
 
 public class MeuLayoutGridBagLayout {
 	public static void main(String[] args) {
-		JButton b1 = new JButton("primeiro");
-		JButton b2 = new JButton("segundo");
-		JButton b3 = new JButton("terceiro");
 		
+		int bola = 0;
+		int x = 1;
+		int vez = 0;
+		
+		JButton b1 = new JButton();
+		JButton b2 = new JButton();
+		JButton b3 = new JButton();
+		JButton b4 = new JButton();
+		JButton b5 = new JButton();
+		JButton b6 = new JButton();
+		JButton b7 = new JButton();
+		JButton b8 = new JButton();
+		JButton b9 = new JButton();
+		
+		GridLayout gl = new GridLayout(3,3);
 		Panel p = new Panel();
-	 
-		GridBagConstraints cons = new GridBagConstraints();
-		GridBagLayout gb = new GridBagLayout();
+		p.setLayout(gl);
 		
-		
- 
-		p.setLayout(gb);
-		p.add(b1,cons);
-		p.add(b2,cons);
-		p.add(b3,cons);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(b5);
+		p.add(b6);
+		p.add(b7);
+		p.add(b8);
+		p.add(b9);
 		
 		JFrame quadro = new JFrame("Nome da minha tela");
 		
@@ -32,7 +43,7 @@ public class MeuLayoutGridBagLayout {
 	 
 		quadro.add(p);
 		quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		quadro.pack();
+		quadro.setSize(700, 400);
 		quadro.setVisible(true);
 		
 		
