eaff98fdef3ab397c07b13970024bcd00bb3869a
Your Name <you@example.com>
2020-09-30 20:12:50 -0300

    borderlayout

--- src/programacao22s_04_layout/MeuLayoutBorderLayout2.java
+++ src/programacao22s_04_layout/MeuLayoutBorderLayout2.java
@@ -1,31 +1,29 @@
 package programacao22s_04_layout;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.Panel;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MeuLayoutFlowLayout {
+public class MeuLayoutBorderLayout2 {
 	public static void main(String[] args) {
 		JButton b1 = new JButton("primeiro");
 		JButton b2 = new JButton("segundo");
 		JButton b3 = new JButton("terceiro");
 		
-		
 		Panel p = new Panel();
-		p.add(b1);
-		p.add(b2);
-		p.add(b3);
-		
-		FlowLayout fl = new FlowLayout();
-		p.setLayout(fl);
+		BorderLayout bl = new BorderLayout(5,5);
+ 
+		p.setLayout(bl);
+		p.add(b1,BorderLayout.NORTH);
+		p.add(b2,BorderLayout.SOUTH);
+		p.add(b3,BorderLayout.EAST);
 		
 		JFrame quadro = new JFrame("Nome da minha tela");
 		
 		
-		
-		
+	 
 		quadro.add(p);
 		quadro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		quadro.pack();
@@ -38,3 +36,10 @@ public class MeuLayoutFlowLayout {
 	}
 
 }
+/*
+		p.setLayout(fl);
+		p.add(b1,BorderLayout.NORTH);
+		p.add(b2,BorderLayout.SOUTH);
+		p.add(b3,BorderLayout.EAST);
+
+*/
