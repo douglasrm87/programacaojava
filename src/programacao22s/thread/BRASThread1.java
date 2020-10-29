8a319a9a97f334d994149a8878630e2531f528b2
Your Name <you@example.com>
2020-10-28 21:18:10 -0300

    threads

---
+++ src/programacao22s/thread/BRASThread1.java
@@ -0,0 +1,50 @@
+package programacao22s.thread;
+
+public class BRASThread1 extends Thread {
+	private static final int _5000 = 8000;
+	private int idBras = 0;
+	private boolean ret = false;
+
+	private boolean fim = false;
+
+	public void run() {
+		validarBras01();
+	}
+
+	public BRASThread1(int idBras) {
+		super();
+		this.idBras = idBras;
+	}
+
+	// equipamento 01
+	private void validarBras01() {
+		System.out.println("Entrei no BRAS1");
+		try {
+			Thread.sleep(_5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("saindo do BRAS1");
+		this.fim = true;
+		switch (this.idBras) {
+		case 1:
+			this.ret = true;
+			return;
+		}
+		this.ret = false;
+		
+	}
+
+	public boolean isRet() {
+		return ret;
+	}
+
+	public boolean isFim() {
+		return fim;
+	}
+
+	public void setFim(boolean fim) {
+		this.fim = fim;
+	}
+
+}
---
+++ src/programacao22s/thread/BRASThread5.java
@@ -0,0 +1,48 @@
+package programacao22s.thread;
+
+public class BRASThread5 extends Thread {
+	private static final int _5000 = 8000;
+	private int idBras = 0;
+	private boolean ret = false;
+	private boolean fim = false;
+
+	public void run() {
+		validarBras05();
+	}
+
+	public BRASThread5(int idBras) {
+		super();
+		this.idBras = idBras;
+	}
+
+	// equipamento 01
+	private void validarBras05() {
+		System.out.println("Entrei no BRAS5");
+		try {
+			Thread.sleep(_5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("saindo do BRAS5");
+		this.fim = true;
+		switch (this.idBras) {
+		case 1:
+			this.ret = true;
+			return;
+		}
+		this.ret = false;
+		
+	}
+
+	public boolean isRet() {
+		return ret;
+	}
+
+	public boolean isFim() {
+		return fim;
+	}
+
+	public void setFim(boolean fim) {
+		this.fim = fim;
+	}
+}
