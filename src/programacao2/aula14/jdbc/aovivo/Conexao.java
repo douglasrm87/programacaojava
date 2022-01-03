7d591f60a04e1769536417ceaadd5a262a6f5222
C049463 <C049463@P030233.copel.nt>
2022-01-03 10:00:08 -0300

    atualizacao
    Change-Id: I1ef34604f4b1213517331eda2eb4dffd455645e4

--- src/programacao2/aula14/jdbc/aovivo/Conexao.java
+++ src/programacao2/aula14/jdbc/aovivo/Conexao.java
@@ -6,7 +6,7 @@ public class Conexao {
 	public static void main(String[] args) {
 		new Conexao().testarConexao();
 	}    
-	
+	 
 	public void testarConexao() {
 		Connection c = conectarPostGree();
 		if (c != null) {
